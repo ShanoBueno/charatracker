7bf4970e902e44edc66fabaa65ae9d6277524d0b
shane <shane.messmer1@gmail.com>
2021-10-20 15:14:01 -0400

    adds login function and password hashing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,9 +49,29 @@ router.post('/', (req, res) => {
   });
 });
 
+router.post('/login', (req, res) => {
+// expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    res.json({ user: dbUserData });
+
+    // Verify user
+
+  });  
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
